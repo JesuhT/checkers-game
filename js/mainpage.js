48cdaa039e14a8bf7ac2286420b3f60935daffe1
JesuhT <eliseotorres1798@gmail.com>
2023-09-28 01:30:23 -0500

    play the game

--- js/mainpage.js
+++ js/mainpage.js
@@ -1,3 +1,49 @@
-$(document).ready(function() {
-       
+$(document).ready(function () {
+    const iframe = $('#checkers');
+    const time = $('.time');
+    const start = $('#gamestart');
+    const cancel = $('#cancel');
+    const play = $('#play');
+    iframe.addClass('unclickable');
+    time.hide();
+    start.hide();
+    cancel.hide();
+    var clicked = false;
+    $('#play').on('click', function () {
+        
+        if (!clicked) {
+            $('.top').hide();
+            $('.item').hide();
+            play.show();
+            time.show();
+            start.show();
+            clicked=true;
+        } else {
+            $('.top').show();
+            $('.item').show();
+            
+            time.hide();
+            start.hide();
+            cancel.hide();
+            clicked=false;
+        }
+
+    })
+
+    start.on('click', function () {
+        iframe.removeClass('unclickable');
+        time.hide();
+        start.hide();
+        cancel.show();
+        play.addClass('unclickable');
+    })
+    cancel.on('click', function () {
+        time.show();
+        start.show();
+        cancel.hide();
+        iframe.addClass('unclickable');
+        iframe.attr('src', iframe.attr('src'));
+        play.removeClass('unclickable');
+    })
+
 })
